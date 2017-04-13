3b28b27c9393d5096ad41a2ca2eb0a6c8becdead
Chris Williams <cwilliams@miamiherald.com>
2017-04-13 13:44:34 -0400

    initialized with default map

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,3 +1,13 @@
-import './modules'
+// FOR DEVELOPMENT
+import 'leaflet/dist/leaflet-src';
 
-console.log(`app.js has loaded!`)
+// FOR PRODUCTION
+// import 'leaflet/dist/leaflet';
+
+var mymap = L.map('mapid').setView([25.7617, -80.1918], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: 'pk.eyJ1IjoiY2hyaXNkdWJ5YSIsImEiOiJjaXRkZDNydnkwMDY4MnRtOTA2Y3hoaGh4In0.kzOT6chkFRIklbX08xiGew'
+}).addTo(mymap);
